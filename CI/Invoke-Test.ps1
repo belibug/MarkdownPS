10557ad1e4ccbb8f51b7f51a95e714e8ccce70a5
Manjunath Beli <61643561+belibug@users.noreply.github.com>
2024-05-29 16:39:08 -0300

    Updated final pester output and PesterConfiguration Object Final Pester result was showing blank because of attribute name change from Pester4 to 5. Fixed it. Changed the way PesterConfiguration is created. All values now lie in single hastable which looks cleaner and easy to troublshoot. Lesser code and slightly more readable.

--- CI/Invoke-Test.ps1
+++ CI/Invoke-Test.ps1
@@ -1,65 +1,69 @@
 param(
-    [Parameter(Mandatory=$true,ParameterSetName="AppVeyor")]
+    [Parameter(Mandatory = $true, ParameterSetName = 'AppVeyor')]
     [switch]$AppVeyor,
-    [Parameter(Mandatory=$false,ParameterSetName="AppVeyor")]
-    [Parameter(Mandatory=$false,ParameterSetName="Console")]
-    [string[]]$Tag=$null,
-    [Parameter(Mandatory=$false,ParameterSetName="AppVeyor")]
-    [Parameter(Mandatory=$false,ParameterSetName="Console")]
-    [string[]]$ExcludeTag=$null,
-    [Parameter(Mandatory=$false,ParameterSetName="AppVeyor")]
-    [Parameter(Mandatory=$false,ParameterSetName="Console")]
-    [switch]$CodeCoverage=$false
+    [Parameter(Mandatory = $false, ParameterSetName = 'AppVeyor')]
+    [Parameter(Mandatory = $false, ParameterSetName = 'Console')]
+    [string[]]$Tag = $null,
+    [Parameter(Mandatory = $false, ParameterSetName = 'AppVeyor')]
+    [Parameter(Mandatory = $false, ParameterSetName = 'Console')]
+    [string[]]$ExcludeTag = $null,
+    [Parameter(Mandatory = $false, ParameterSetName = 'AppVeyor')]
+    [Parameter(Mandatory = $false, ParameterSetName = 'Console')]
+    [switch]$CodeCoverage = $false
 )
-$srcPath=Resolve-Path -Path "$PSScriptRoot\..\Src"
-$outputFile=[System.IO.Path]::GetTempFileName()+".xml"
+$srcPath = Resolve-Path -Path "$PSScriptRoot\..\Src"
+$outputFile = [System.IO.Path]::GetTempFileName() + '.xml'
+$codeCoveragePath = $outputFile.Replace('.xml', '.codecoverage.xml')
 
-# https://pester-docs.netlify.app/docs/commands/New-PesterConfiguration
-$pesterConfiguration=New-PesterConfiguration
-$pesterConfiguration.Run.Path=$srcPath
-$pesterConfiguration.Run.PassThru=$true
-$pesterConfiguration.Run.Path=$srcPath
-$pesterConfiguration.Filter.Tag=$Tag
-$pesterConfiguration.Filter.ExcludeTag=$ExcludeTag
-
-$pesterConfiguration.TestResult.Enabled=$true
-$pesterConfiguration.TestResult.OutputFormat="NUnitXml"
-$pesterConfiguration.TestResult.OutputPath=$outputFile
-
-$pesterConfiguration.Output.Verbosity="Detailed"
-#$pesterConfiguration.Output.Verbosity="Diagnostic"
-
-if($CodeCoverage)
-{
-    $pesterConfiguration.CodeCoverage.Enabled=$true
-#    $pesterConfiguration.CodeCoverage.OutputFormat="JaCoCo"
-    $pesterConfiguration.CodeCoverage.OutputPath=$outputFile.Replace(".xml",".codecoverage.xml")
-}
-
-$pesterResult=Invoke-Pester -Configuration $pesterConfiguration
-if($CodeCoverage)
-{
-    $pesterResult|Select-Object @{
-        Name="CommandsAnalyzed"
-        Expression={$_.CodeCoverage.NumberOfCommandsAnalyzed}
-    },@{
-        Name="FilesAnalyzed"
-        Expression={$_.CodeCoverage.NumberOfFilesAnalyzed}
-    },@{
-        Name="CommandsExecuted"
-        Expression={$_.CodeCoverage.NumberOfCommandsExecuted}
-    },@{
-        Name="CommandsMissed"
-        Expression={$_.CodeCoverage.NumberOfCommandsMissed}
+$pesterConfiguration = [PesterConfiguration]@{
+    Run          = @{
+        Path     = $srcPath
+        PassThru = $true
+    }
+    TestResult   = @{
+        Enabled      = $true
+        OutputFormat = 'NUnitXml'
+        OutputFile   = $outputFile
+    }
+    Filter       = @{
+        Tag        = $tag
+        ExcludeTag = $ExcludeTag
+    }
+    Should       = @{
+        ErrorAction = 'Continue'
+    }
+    Output       = @{
+        Verbosity = 'Detailed'
+    }
+    CodeCoverage = @{
+        Enabled    = [bool]$CodeCoverage
+        OutputPath = $codeCoveragePath
     }
 }
 
-switch($PSCmdlet.ParameterSetName) {
+$pesterResult = Invoke-Pester -Configuration $pesterConfiguration
+if ($CodeCoverage) {
+    $pesterResult | Select-Object @{
+        Name       = 'CommandsAnalyzed'
+        Expression = { $_.CodeCoverage.CommandsAnalyzedCount }
+    }, @{
+        Name       = 'FilesAnalyzed'
+        Expression = { $_.CodeCoverage.FilesAnalyzedCount }
+    }, @{
+        Name       = 'CommandsExecuted'
+        Expression = { $_.CodeCoverage.CommandsAnalyzedCount }
+    }, @{
+        Name       = 'CommandsMissed'
+        Expression = { $_.CodeCoverage.CommandsMissedCount }
+    }
+}
+
+switch ($PSCmdlet.ParameterSetName) {
     'AppVeyor' {
         (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", $outputFile)
         if ($pesterResult.FailedCount -gt 0) { 
             throw "$($pesterResult.FailedCount) tests failed."
-       }        
+        }        
     }
     'Console' {
 
